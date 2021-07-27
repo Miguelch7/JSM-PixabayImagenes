3527df0b142c49308fa8edb98004e8d8e6d5142d
Miguel <chanampamiguel@gmail.com>
2021-07-27 20:01:20 -0300

    Utilizando Async Await

--- js/app.js
+++ js/app.js
@@ -45,16 +45,18 @@ function mostrarAlerta(mensaje) {
     }
 }
 
-function buscarImagenes(termino) {
+async function buscarImagenes(termino) {
     const key = '22488837-170c62b5e8b0025a8350da0bd';
     const url = `https://pixabay.com/api/?key=${key}&q=${termino}&per_page=${registrosPorPagina}&page=${paginaActual}`;
-
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(resultado => {
-            totalPaginas = calcularPaginas(resultado.totalHits);
-            mostrarImagenes(resultado.hits);
-        })    
+      
+    try {
+        const respuesta = await fetch(url);
+        const resultado = await respuesta.json();
+        totalPaginas = calcularPaginas(resultado.totalHits);
+        mostrarImagenes(resultado.hits);
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 // Generador que va a registrar la cantidad de elementos de acuerdo a las páginas
